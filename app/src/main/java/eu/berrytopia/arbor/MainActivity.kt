10895f57346032ceb2bc392fa913cb545776dad8
Chantal Burkhard <chante11@t-online.de>
2022-09-15 15:45:27 +0200

    added map view

--- app/src/main/java/eu/berrytopia/arbor/MainActivity.kt
+++ app/src/main/java/eu/berrytopia/arbor/MainActivity.kt
@@ -1,11 +1,51 @@
 package eu.berrytopia.arbor
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var RECORD_REQUEST_CODE = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!Environment.isExternalStorageManager()) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+            val uri: Uri = Uri.fromParts("package", this.packageName, null)
+            intent.data = uri
+            startActivity(intent)
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val gpsPermissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            ActivityCompat.requestPermissions(this, gpsPermissions, RECORD_REQUEST_CODE)
+        }
+
+        val buttonClick = findViewById<Button>(R.id.loginBtn)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/eu/berrytopia/arbor/MapActivity.kt
+++ app/src/main/java/eu/berrytopia/arbor/MapActivity.kt
@@ -1,17 +1,102 @@
 package eu.berrytopia.arbor
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.os.Looper
 import android.os.PersistableBundle
+import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 
 
 class MapActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setSupportActionBar(findViewById(R.id.my_toolbar))
 
+    override fun onCreate(savedInstanceState: Bundle?/*, persistentState: PersistableBundle?*/) {
+        super.onCreate(savedInstanceState/*, persistentState*/)
+        setContentView(R.layout.map_activity)
+        //setSupportActionBar(findViewById(R.id.my_toolbar))
 
+        initMap()
+        initGps()
+    }
+
+    // Geofence plantation
+    val fence =
+        listOf(
+            GeoPoint(49.948457, 7.936816), //top left
+            GeoPoint(49.948420, 7.937085), //top right
+            GeoPoint(49.947366, 7.937146), //bottom right
+            GeoPoint(49.947454, 7.936883) //bottom left
+        )
+
+    var map: MapView? = null
+
+    private fun initMap() {
+        Configuration.getInstance().userAgentValue = applicationContext.packageName
+
+        map = findViewById(R.id.map)
+
+        map!!.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        map!!.controller.setZoom(19.0)
+
+        updateMapPosition(GeoPoint(49.950314, 7.9370000))
+
+        val poly = org.osmdroid.views.overlay.Polygon(map!!)
+        poly.points = fence
+
+        map!!.overlays.add(poly)
+    }
+
+    lateinit var client: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
+    private fun initGps() {
+        client = LocationServices.getFusedLocationProviderClient(this)
+        client.requestLocationUpdates(
+            LocationRequest().setInterval(1000).setPriority(Priority.PRIORITY_HIGH_ACCURACY),
+            locationCallBack,
+            Looper.myLooper()
+        )
+    }
+
+    var currentLat: String = ""
+    var currentLong: String = ""
+
+    var locationCallBack = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult ?: return
+            for (it in locationResult.locations) {
+                if (currentLat != "%.4f".format(it.latitude) ||
+                    currentLong != "%.4f".format(it.longitude)) {
+
+                    currentLat = "%.4f".format(it.latitude)
+                    currentLong = "%.4f".format(it.longitude)
+                    Log.i("MYTEST", "Lat: ${it.latitude}")
+                    Log.i("MYTEST", "Lon: ${it.longitude}")
+
+                    runOnUiThread {
+                        updateMapPosition(GeoPoint(it.latitude, it.longitude))
+                    }
+                }
+            }
+        }
+    }
+
+    fun updateMapPosition(myPos: GeoPoint) {
+        map!!.controller.setCenter(myPos)
+        map!!.controller.setZoom(19.0)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
