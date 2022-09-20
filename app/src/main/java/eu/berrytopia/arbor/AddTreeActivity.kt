959ef5df3caa308f9de4b2391056bebd4fd5a5bc
Chantal Burkhard <chante11@t-online.de>
2022-09-20 14:10:55 +0200

    added camera function for addTree and addEvent

--- app/src/main/java/eu/berrytopia/arbor/AddTreeActivity.kt
+++ app/src/main/java/eu/berrytopia/arbor/AddTreeActivity.kt
@@ -1,11 +1,23 @@
 package eu.berrytopia.arbor
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class AddTreeActivity: AppCompatActivity() {
+    var cameraRequest = 1888
+    lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.addtree_activity)
@@ -18,6 +30,25 @@ class AddTreeActivity: AppCompatActivity() {
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
             spinner.adapter = adapter
+        }
+
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequest)
+        imageView = findViewById(R.id.imageHolder)
+        val photoBtn: Button = findViewById(R.id.photoEventBtn2)
+
+        photoBtn.setOnClickListener{
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == cameraRequest) {
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(photo)
         }
     }
 }
